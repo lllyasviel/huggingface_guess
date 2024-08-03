aebabb94eaaa1a26a3b37128d1c079838c134623
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-08-03 16:19:22 -0700

    Update __init__.py

--- huggingface_guess/__init__.py
+++ huggingface_guess/__init__.py
@@ -1,8 +1,14 @@
 from huggingface_guess.detection import model_config_from_unet, unet_prefix_from_state_dict, model_config_from_diffusers_unet
 
 
-def guess_repo_name(state_dict):
+def guess(state_dict):
     unet_key_prefix = unet_prefix_from_state_dict(state_dict)
-    model_config = model_config_from_unet(state_dict, unet_key_prefix, use_base_if_no_match=True)
-    repo_id = model_config.huggingface_repo
+    result = model_config_from_unet(state_dict, unet_key_prefix, use_base_if_no_match=False)
+    return result
+
+
+def guess_repo_name(state_dict):
+    config = guess(state_dict)
+    assert config is not None
+    repo_id = config.huggingface_repo
     return repo_id
