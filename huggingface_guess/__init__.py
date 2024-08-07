3f96b28763515dbe609792135df3615a440c66dc
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-08-06 22:51:02 -0700

    Update __init__.py

--- huggingface_guess/__init__.py
+++ huggingface_guess/__init__.py
@@ -5,6 +5,10 @@ def guess(state_dict):
     unet_key_prefix = unet_prefix_from_state_dict(state_dict)
     result = model_config_from_unet(state_dict, unet_key_prefix, use_base_if_no_match=False)
     result.unet_key_prefix = [unet_key_prefix]
+    if 'image_model' in result.unet_config:
+        del result.unet_config['image_model']
+    if 'audio_model' in result.unet_config:
+        del result.unet_config['audio_model']
     return result
 
 
